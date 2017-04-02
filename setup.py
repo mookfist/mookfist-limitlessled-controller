0cefa3f2c416826071c6ae0fe3c95de531d8fea1
v0idnull <v0idnull@localhost>
2017-04-02 16:02:30 -0400

    ver 0.0.4 tweaks for pypi

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 import os
 from setuptools import setup
 
+desc = open('README.md').read()
+
 setup(
     name="mookfist-lled-controller",
-    version="0.0.3",
+    description=desc,
+    version="0.0.4",
     author="mookfist",
     author_email="mookfist@gmail.com",
     url="https://github.com/mookfist/mookfist-lled-controller",
@@ -12,5 +15,14 @@ setup(
     install_requires=[
         'docopt',
         'colorama'
-    ]
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3'
+    ],
+    keywords=['milight','limitlessled']
 )
