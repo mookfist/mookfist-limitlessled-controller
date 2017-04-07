d20c18390375e729f57b8f775196b71c5f119f98
mookfist <mookfist@gmail.com>
2017-04-06 21:29:01 -0400

    OH YEAH! packages is not recursive *LE SIGH*

--- setup.py
+++ setup.py
@@ -15,12 +15,12 @@ Bridge Version Support:
 setup(
     name="mookfist-lled-controller",
     description=__doc__,
-    version="0.0.7",
+    version="0.0.8",
     author="mookfist",
     author_email="mookfist@gmail.com",
     url="https://github.com/mookfist/mookfist-lled-controller",
     scripts=['lled.py'],
-    packages=['mookfist_lled_controller'],
+    packages=['mookfist_lled_controller','mookfist_lled_controller.bridges'],
     install_requires=[
         'docopt',
         'colorama'
